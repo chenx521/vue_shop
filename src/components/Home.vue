44c8b23235343288f5d063d02ca8c967e7fd6a03
chenx521 <ch19086@163.com>
2021-02-20 22:57:31 +0800

    login

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        Home组件
+        <el-button type="info" @click="logout">退出</el-button>
+
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        logout () {
+            /* 清空token信息 */
+            window.sessionStorage.clear()
+            this.$router.push('/login')
+            console.log('jasd')
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
